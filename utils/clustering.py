c172e931ec9be6fc4dbe7a272ac56d42f4aff786
jchou3 <jocelynchou@gmail.com>
2023-09-22 22:02:40 +0200

    update algo

--- utils/clustering.py
+++ utils/clustering.py
@@ -4,20 +4,17 @@ import math
 
 class cluster:
 
-    def __init__(self, words: list):
+    def __init__(self, words: list, model):
         self.words = words
+        self.model = model
 
-    # Calculates distance between 2 vector coordinates
-    def get_distance(self):
+    # Calculates distance value of cluster
+    def get_val(self):
         pass
 
     # Calculates center of a given group
     # Returns vector coordinates
     def get_center(self):
-        pass
-
-    # Returns vector of given word
-    def get_vector(self):
         pass
 
     def get_words(self):
@@ -28,5 +25,27 @@ class cluster:
 # Adjusted to avoid assasin
 # Returns list of clusters
 
-def agg_cluster(words: cluster, assasin: str, min_clusters = None, max_clusters = None):
+def agg_cluster(words: list, assasin: str, model, min_clusters = None, max_clusters = None):
+
+    assasin_vec = model.wv[assasin]
+    clusters = []
+
+
+    #Fusion is determined by disimilarity between groups -- pick a height and cut it off
+    for word in words:
+        clusters.append(cluster(words, model))
+
+
+def find_closest(clusters: list):
     pass
+
+    
+
+# Returns vector of given word
+def get_vector(word, model):
+    pass
+
+# Returns distance between two points
+def get_distance(a, b, model):
+    pass
+
